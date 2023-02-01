bffedf309deefebcf6146c01c5ba9fc8f88bc8e1
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2023-02-02 00:07:46 +0100

    Add provider block

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1" # Frankfurt
+  # uses environment variables in Terraform Cloud
+}
